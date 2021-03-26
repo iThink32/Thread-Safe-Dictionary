29cdaf00464213d4be5ad9f4e014b7f3e2b6b14e
Shashank <34152717+iThink32@users.noreply.github.com>
2021-03-26 16:25:59 +0530

    Key: Hashable implementation
    - Made the dictionary accept any key that conforms to Hashable

--- ThreadSafeDictionary.swift
+++ ThreadSafeDictionary.swift
@@ -4,30 +4,30 @@
 //  Created by Shashank on 29/10/20.
 //
 
-class ThreadSafeDictionary<T>: Collection {
+class ThreadSafeDictionary<V: Hashable,T>: Collection {
 
-    private var dictionary: [String: T]
+    private var dictionary: [V: T]
     private let concurrentQueue = DispatchQueue(label: "Dictionary Barrier Queue",
                                                 attributes: .concurrent)
-    var startIndex: Dictionary<String, T>.Index {
+    var startIndex: Dictionary<V, T>.Index {
         return self.dictionary.startIndex
     }
 
-    var endIndex: Dictionary<String, T>.Index {
+    var endIndex: Dictionary<V, T>.Index {
         return self.dictionary.endIndex
     }
 
-    init(dict: [String: T] = [String: T]()) {
+    init(dict: [V: T] = [V:T]()) {
         self.dictionary = dict
     }
     // this is because it is an apple protocol method
     // swiftlint:disable identifier_name
-    func index(after i: Dictionary<String, T>.Index) -> Dictionary<String, T>.Index {
+    func index(after i: Dictionary<V, T>.Index) -> Dictionary<V, T>.Index {
         return self.dictionary.index(after: i)
     }
     // swiftlint:enable identifier_name
 
-    subscript(key: String) -> T? {
+    subscript(key: V) -> T? {
         set(newValue) {
             self.concurrentQueue.async(flags: .barrier) {[weak self] in
                 self?.dictionary[key] = newValue
@@ -41,7 +41,7 @@ class ThreadSafeDictionary<T>: Collection {
     }
 
     // has implicity get
-    subscript(index: Dictionary<String, T>.Index) -> Dictionary<String, T>.Element {
+    subscript(index: Dictionary<V, T>.Index) -> Dictionary<V, T>.Element {
         self.concurrentQueue.sync {
             return self.dictionary[index]
         }
